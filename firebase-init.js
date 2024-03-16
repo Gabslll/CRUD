3d3029f7fac9f4cb42151ef8e8e6e1a8b0308b87
Gabriel Ramos <gabrielrammus42@gmail.com>
2024-03-15 22:10:33 -0300

    Creating validation register account

---
+++ firebase-init.js
@@ -0,0 +1,10 @@
+
+const firebaseConfig = {
+    apiKey: "AIzaSyA738-Nmf62pn0wLzq_hH2iiVXDhEeEex8",
+    authDomain: "controle-de-gastos-19446.firebaseapp.com",
+    projectId: "controle-de-gastos-19446",
+    storageBucket: "controle-de-gastos-19446.appspot.com",
+    messagingSenderId: "1021244677624",
+    appId: "1:1021244677624:web:e9737e371302e621d24a0d"
+};
+firebase.initializeApp(firebaseConfig);
